0d59c03e06dcd9055a3bd842570b9811517ee9b4
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-07-20 12:45:39 +0100

    Created DB Context
    Added database context, ready for table creation

---
+++ Data/DungeonsDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dungeons.Data
+{
+    public class DungeonsDBContext
+    {
+    }
+}
