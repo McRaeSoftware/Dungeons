0986ff0a4c90bfc1bab9abc61cd0a72010e0737a
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-07-23 19:05:05 +0100

    Created Initial Bag class
    Created Initial Bag class

---
+++ Models/Bag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dungeons.Models
+{
+    public class Bag
+    {
+        [Required]
+        public int CharacterCode { get; set; }
+
+        [Required]
+        public List<string> ItemList;
+
+        [Required]
+        [DisplayName("gp")]
+        public int Gold { get; set; }
+
+        [Required]
+        [DisplayName("sp")]
+        public int Silver { get; set; }
+
+        [Required]
+        [DisplayName("cp")]
+        public int Copper { get; set; }
+
+
+    }
+}
