de2b09a58bf505d4bdf713e184fb88f4cbf200df
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-07-23 19:04:50 +0100

    Created initial Equipment Class
    Created initial Equipment Class

---
+++ Models/Equipped.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dungeons.Models
+{
+    public class Equipped
+    {
+        [Required]
+        public string CharacterCode { get; set; }
+
+        [Required]
+        public List<string> SpecialItems { get; set; }
+
+        [Required]
+        public string ArmourName { get; set; }
+
+        [Required]
+        public string MainHand { get; set; }
+
+        [Required]
+        public string OffHand { get; set; }
+    }
+}
