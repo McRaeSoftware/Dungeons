f072180bb0be21dfa3ce6399923262d7aa130256
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-07-23 13:41:00 +0100

    Created PC (playable character) class
    created PC class as a templte so far

---
+++ Models/PC.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dungeons.Models
+{
+    public class PC
+    {
+        [Key]
+        public int Character_ID { get; set; }
+
+        [Required]
+        public int User_ID { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Alignment { get; set; }
+
+        [Required]
+        [DisplayName("Exp")]
+        public int Experience { get; set; }
+
+        [Required]
+        public int Level { get; set; }
+
+        [Required]
+        [DisplayName("Race")]
+        public string RaceName { get; set; }
+
+        [Required]
+        [DisplayName("Class")]
+        public string ClassName { get; set; }
+
+        [Required]
+        [DisplayName("AC")]
+        public int ArmourClass { get; set; }
+
+        [Required]
+        public int MaxHealth { get; set; }
+
+        [Required]
+        public int CurrentHealth { get; set; }
+
+        [Required]
+        public int Strength { get; set; }
+
+        [Required]
+        public int Dexterity { get; set; }
+
+        [Required]
+        public int Constitution { get; set; }
+
+        [Required]
+        public int Intelligence { get; set; }
+
+        [Required]
+        public int Wisdom { get; set; }
+
+        [Required]
+        public int Charisma { get; set; }
+
+        [Required]
+        public string SavingThrows { get; set; }
+
+        [Required]
+        public string Proficiencies { get; set; }
+
+        [Required]
+        public string Languages { get; set; }
+
+    }
+}
