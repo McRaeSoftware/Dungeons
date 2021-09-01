99ae2cb834d34e1d5219ad41e2f6e993655ca0c1
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-09-01 13:04:31 +0100

    Update CharacterController.cs
    Now changed all IActionresult methods to the async Auto generated methods instead.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,43 +1,153 @@
-﻿using Dungeons.Data;
-using Dungeons.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Dungeons.Data;
+using Dungeons.Models;
 
 namespace Dungeons.Controllers
 {
     public class CharacterController : Controller
     {
-        private readonly DungeonsDBContext _database;
+        private readonly DungeonsDBContext _context;
 
-        public CharacterController(DungeonsDBContext database)
+        public CharacterController(DungeonsDBContext context)
         {
-            _database = database;
+            _context = context;
         }
 
-        public IActionResult ManageCharacter()
+        // GET: Character
+        public async Task<IActionResult> Index()
         {
-            IEnumerable<Character> characterList = _database.Character;
-            return View(characterList);
+            return View(await _context.Character.ToListAsync());
         }
 
-        public IActionResult CreateCharacter()
+        // GET: Character/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Character
+                .FirstOrDefaultAsync(m => m.Character_ID == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return View(character);
+        }
+
+        // GET: Character/Create
+        public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Character/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult CreateCharacter(Character character)
+        public async Task<IActionResult> Create([Bind("Character_ID,User_ID,Code,Name,Alignment,Experience,Level,RaceName,ClassName,MaxHealth,CurrentHealth,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,SavingThrows,Proficiencies,Languages")] Character character)
         {
             if (ModelState.IsValid)
             {
-                _database.Character.Add(character);
-                _database.SaveChanges();
+                _context.Add(character);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            return View();
+            return View(character);
+        }
+
+        // GET: Character/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Character.FindAsync(id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Character/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Character_ID,User_ID,Code,Name,Alignment,Experience,Level,RaceName,ClassName,MaxHealth,CurrentHealth,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,SavingThrows,Proficiencies,Languages")] Character character)
+        {
+            if (id != character.Character_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(character);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CharacterExists(character.Character_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(character);
+        }
+
+        // GET: Character/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Character
+                .FirstOrDefaultAsync(m => m.Character_ID == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return View(character);
+        }
+
+        // POST: Character/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var character = await _context.Character.FindAsync(id);
+            _context.Character.Remove(character);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CharacterExists(int id)
+        {
+            return _context.Character.Any(e => e.Character_ID == id);
         }
     }
 }
