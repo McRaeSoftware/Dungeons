d4a6d8619189fd13b5111114b98f4b20143b3bcc
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-09-16 20:43:27 +0100

    Create RegularExpressionList.cs
    Created a regular expression attribute which checks each element in a list based on a given patter.

---
+++ Models/RegularExpressionList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dungeons.Models
+{
+    public class RegularExpressionListAttribute : RegularExpressionAttribute
+    {
+        public RegularExpressionListAttribute(string pattern) : base(pattern) { }
+
+        public override bool IsValid(object value)
+        {
+            if(value is not IEnumerable<string>)
+            {
+                return false;
+            }
+
+            foreach (var val in value as IEnumerable<string>)
+            {
+                if (!Regex.IsMatch(val, Pattern))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
