1b6bc8ef8f253fce531fc48706a85b7e91cb9295
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-09-24 14:44:54 +0100

    Update CharacterEquipped.cs
    REGEX for Characters equipped items.

--- Models/CharacterEquipped.cs
+++ Models/CharacterEquipped.cs
@@ -13,19 +13,29 @@ namespace Dungeons.Models
         public string CharacterCode { get; set; }
 
         [Required]
+        [RegularExpressionList(@"^[a-zA-Z ']+$",
+            ErrorMessage = "Special Items name must only contain letters, spaces and apostrophes")]
         public List<string> SpecialItems { get; set; }
 
         [Required]
         [DisplayName("AC")]
+        [RegularExpression(@"^[0-9]{1,2}$",
+            ErrorMessage = "Armour class must be a number")]
         public int ArmourClass { get; set; }
 
         [Required]
+        [RegularExpressionList(@"^[a-zA-Z ']+$",
+            ErrorMessage = "Armour name must only contain letters, spaces and apostrophes")]
         public string ArmourName { get; set; }
 
         [Required]
+        [RegularExpressionList(@"^[a-zA-Z ']+$",
+            ErrorMessage = "Main hand must only contain letters, spaces and apostrophes")]
         public string MainHand { get; set; }
 
         [Required]
+        [RegularExpressionList(@"^[a-zA-Z ']+$",
+            ErrorMessage = "Offhand must only contain letters, spaces and apostrophes")]
         public string OffHand { get; set; }
     }
 }
