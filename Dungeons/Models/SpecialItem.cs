c331521f3d1657feb66a689d6881ec5a9d29fe17
ajhay709 <ajhay709@gmail.com>
2021-11-12 10:01:19 +0000

    Created a new SpecialItems class to hold information on special items, as this was causing an issue with data types in the CharacterEquipped class.

---
+++ Dungeons/Models/SpecialItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dungeons.Models
+{
+    public class SpecialItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public bool Attunement { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
