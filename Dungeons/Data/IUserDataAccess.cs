fcf344da52c9eeab27152aeb4402f96d9bf86750
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-11-12 12:42:21 +0000

    Changed order of methods

--- Dungeons/Data/IUserDataAccess.cs
+++ Dungeons/Data/IUserDataAccess.cs
@@ -6,8 +6,8 @@ namespace Dungeons.Data
 {
     public interface IUserDataAccess
     {
-        Task<User> GetUserByID(int id);
         Task<List<User>> GetUserListAsync();
+        Task<User> GetUserByID(int id);
         Task<bool> CreateUser(User user);
         Task<bool> UpdateUser(User user);
         Task<bool> DeleteUser(int id);
